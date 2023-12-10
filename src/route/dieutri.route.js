6152f24a189bd3286e8416e3c4c0319fcb733500
Toan Golden Pham <toanphamphu2003@gmail.com>
2023-12-10 15:39:47 +0700

    fix loi j do

--- src/route/dieutri.route.js
+++ src/route/dieutri.route.js
@@ -1,9 +1,9 @@
-const controller = require('../controller/donthuoc.controller')
+const controller = require('../controller/dieutri.controller')
 const express = require('express')
 const route = express.Router()
 
-route.get('/dieutri/ListBDT/benhnhan/:id', controller.getlistBDTbyID)
-route.get('/dieutri/ListBDT/date/?dateA&dateB', controller.getlistBDTbyDate)
+route.get('/dieutri/ListBDT/benhnhan/:id', controller.getListBDTbyID)
+route.get('/dieutri/ListBDT/date/?dateA&dateB', controller.getListBDTbyDate)
 route.get('/dieutri/KeHoach/:id', controller.getKeHoach)
 route.get('/dieutri/BDT/:id', controller.getBDT)
 
