052da81c4dde4ab8eab3e1fde7ce4b07319ec723
ZzHopezZ <trananhoanguyen@gmail.com>
2023-12-16 23:56:21 +0700

    chi tiet dt

--- src/model/dieutri.model.js
+++ src/model/dieutri.model.js
@@ -27,6 +27,41 @@ function te(dateA,dateB) {
     }
 }
 
+async function returnAddChiTietDT(madieutri, idbuoidieutri)
+{
+    let request = new sql.Request()
+
+    request.input('MADIEUTRI',sql.Char,madieutri)
+    .input('IDBUOIDIEUTRI',sql.Char, idbuoidieutri)
+
+    let isSuccess = await request.execute(`THEMCHITIETDT`)
+    if (isSuccess.returnValue != 1)
+    {
+        
+        return true
+    }
+
+    return false
+}
+async function returnRangDT(madieutri, iddieutri, tenrang, matdieutri)
+{
+    let request = new sql.Request()
+
+    request.input('MADIEUTRI', madieutri)
+            .input('IDBUOIDIEUTRI', iddieutri)
+            .input('TENRANG',sql.NChar, tenrang)
+            .input('MATDIEUTRI', matdieutri)
+
+    let isSuccess = await request.execute(`THEMRANGDT`)
+    if (isSuccess.returnValue != 1)
+    {
+        
+        return true
+    }
+
+    return false
+}
 module.exports = {
     te,
+    returnAddChiTietDT
 }
